dfb526e35f240d7c9b1fb3f624857a5f716bbbb5
Jeremy Wood <jeremy.wood@7factor.io>
2022-09-22 10:59:06 -0400

    Replace s3 tf with non-deprecated resources.

--- s3.tf
+++ s3.tf
@@ -1,14 +1,28 @@
 resource "aws_s3_bucket" "web" {
   bucket = var.primary_fqdn
+}
 
-  acl = "public-read"
+resource "aws_s3_bucket_acl" "web_acl" {
+  bucket = aws_s3_bucket.web.id
+  acl    = "public-read"
+}
 
-  website {
-    index_document = var.web_index_doc
-    error_document = var.web_error_doc
+resource "aws_s3_bucket_website_configuration" "web_site_configuration" {
+  bucket = aws_s3_bucket.web.id
 
-    routing_rules = var.routing_rules
+  index_document {
+    suffix = var.web_index_doc
   }
+
+  error_document {
+    key = var.web_error_doc
+  }
+
+  routing_rules = var.routing_rules
+}
+
+resource "aws_s3_bucket_cors_configuration" "web_cors_configuration" {
+  bucket = aws_s3_bucket.web.id
 
   cors_rule {
     allowed_headers = ["*"]
@@ -17,7 +31,10 @@ resource "aws_s3_bucket" "web" {
     expose_headers  = var.cors_expose_headers
     max_age_seconds = var.cors_max_age_seconds
   }
+}
 
+resource "aws_s3_bucket_policy" "web_policy" {
+  bucket = aws_s3_bucket.web.id
   policy = <<EOF
 {
     "Version": "2012-10-17",
@@ -43,6 +60,4 @@ resource "aws_s3_bucket" "web" {
     ]
 }
 EOF
-
 }
-
